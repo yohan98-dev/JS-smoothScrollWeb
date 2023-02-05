d4de8b6bb6b3540d3537491500b053a23d8b65af
Avishka Yohan <60248593+yohan98-dev@users.noreply.github.com>
2023-02-05 13:31:24 +0530

    JS for dyanamic Date and Nav bar

--- assets/app.js
+++ assets/app.js
@@ -4,8 +4,31 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
 
 // ********** close links ************
+const navToggle = document.querySelector('.nav-toggle');
+const linksContainer = document.querySelector('.links-container');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function(){
+// linksContainer.classList.toggle('show-links');
+
+
+//adding dynamic height to the nav bar    
+const containerHeght= linksContainer.getBoundingClientRect().height; 
+const linksHeight = links.getBoundingClientRect().height;
+
+if(containerHeght === 0){
+    linksContainer.style.height = `${linksHeight}px`;
+}
+else{
+    linksContainer.style.height = 0;
+}
+});
+
+
 
 // ********** fixed navbar ************
 
